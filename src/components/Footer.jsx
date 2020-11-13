2114d12515ab4b8fd1d34229a4616b6fa6d1a317
jisel otavo <ladyotavo@gmail.com>
2020-11-13 11:01:12 -0500

    se agrega imagenes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+const Footer = () => (
+    <footer className="footer">
+        <a href="/">Terminos y condiciones</a>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => (
+    <header className="header">
+        <p>Header</p>
+    </header>
+);
+
+export default Header;
